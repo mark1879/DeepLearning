ba95f87ed829ff6a411def09626fe65e2615c098
MarkWu <mark1879@163.com>
2023-02-27 22:29:45 +0800

    add "FeedforwardNeuralNetwork" model

--- PytorchPractice/FeedforwardNeuralNetwork/feedforward_neural_network.py
+++ PytorchPractice/FeedforwardNeuralNetwork/feedforward_neural_network.py
@@ -3,10 +3,13 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 input_size = 784
+hidden_size = 500
 num_classes = 10
 num_epochs = 1
-batch_size = 5
+batch_size = 100
 learning_rate = 0.001
 
 train_dataset = torchvision.datasets.MNIST(root='../data/minist',
@@ -26,14 +29,31 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=False)
 
-model = nn.Linear(input_size, num_classes)
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+
+model = NeuralNet(input_size, hidden_size, num_classes).to(device)
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 total_step = len(train_loader)
+
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
-        images = images.reshape(-1, 28 * 28)
+        images = images.reshape(-1, 28 * 28).to(device)
+        labels = labels.to(device)
 
         outputs = model(images)
         loss = criterion(outputs, labels)
@@ -50,16 +70,13 @@ with torch.no_grad():
     correct = 0
     total = 0
     for images, labels in test_loader:
-        images = images.reshape(-1, 28*28)
+        images = images.reshape(-1, 28 * 28).to(device)
+        labels = labels.to(device)
         outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
+        _, pridicted = torch.max(outputs.data, 1)
         total += labels.size(0)
-        correct += (predicted == labels).sum()
+        correct += (pridicted == labels).sum().item()
 
-    print('Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+    print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
 
 torch.save(model.state_dict(), 'model.ckpt')
-
-
-
-
